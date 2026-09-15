d07f78c39bd4708d4a92fe0aa661805278eea382
Adityaloharr0030 <lohar6987@gmail.com>
2026-09-15 22:18:54 +0530

    Add main thread delay example

--- src/Preactice_qution/MyThread.java
+++ src/Preactice_qution/MyThread.java
@@ -13,10 +13,15 @@ public class MyThread extends Thread{
         }
 }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         MyThread t1=new MyThread("t1");
         MyThread t2=new MyThread("t2");
         t1.start();
         t2.start();
+        for (int i = 1; i <= 3; i++) {
+            System.out.println(i);
+            Thread.sleep(1000);
+        }
+
     }
 }
